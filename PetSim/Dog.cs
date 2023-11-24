c7c39e701127a13c7f58d4b0f471ba60f48efad2
Jan Švimberský <jan.svimbersky@cmis.cz>
2023-11-24 09:59:40 +0100

    Add project files.

---
+++ PetSim/Dog.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSim
+{
+    public class Dog
+    {
+        private int hunger;
+        public int Hunger
+        {
+            private set
+            {
+                hunger = value;
+
+                CheckHunger();
+            }
+            get
+            {
+                return hunger;
+            }
+        }
+
+        public string Name { get; init; }
+
+        public Dog(string name)
+        {
+            this.Name = name;
+        }
+        
+        public int Feed()
+        {
+            Console.WriteLine("Eating...");
+
+            return Hunger -= 20;
+        }
+
+        public int Play()
+        {
+            Console.WriteLine("Playing...");
+
+            return Hunger += 30;
+        }
+
+        private void CheckHunger()
+        {
+            if (hunger > 40 && hunger <= 100)
+            {
+                Console.WriteLine("Hungry!");
+                return;
+            }
+
+            if (hunger > 100)
+            {
+                Console.WriteLine("I DIEEEEED!!!");
+                return;
+            }
+        }
+    }
+}
---
+++ PetSim/Program.cs
@@ -0,0 +1,11 @@
+﻿using PetSim;
+
+var dog = new Dog("Vašek");
+
+dog.Feed();
+
+dog.Play();
+dog.Play();
+dog.Play();
+dog.Play();
+dog.Play();
